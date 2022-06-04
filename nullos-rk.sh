cd8812f9b20b1dda8807e1139e665f2bd0eadb9c
David Konsumer <konsumer@jetboystudio.com>
2022-06-03 19:00:47 -0700

    safety, download boot zip, cache downloads

--- nullos-rk.sh
+++ nullos-rk.sh
@@ -1,52 +1,58 @@
 #!/bin/bash -e
 
+# TODO: check to make sure it's running in linux
+
+# deps
+# TODO: check to make sure it's running in deb-based linux
+sudo apt install -y build-essential debootstrap unzip git dosfstools qemu-utils
+
 DISKFILE="nullos-rk-$(date +"%m-%d-%Y").qcow2"
+DEVICE_NBD="/dev/nbd0"
 
-DIR=$PWD
+DIR_OUT="${PWD}"
 
+# dowload prebuilt mali drivers
+if [ ! -f "${DIR_OUT}/rk3326_r13p0_gbm_with_vulkan_and_cl.zip" ];then
+  wget https://dn.odroid.com/RK3326/ODROID-GO-Advance/rk3326_r13p0_gbm_with_vulkan_and_cl.zip -O "${DIR_OUT}/rk3326_r13p0_gbm_with_vulkan_and_cl.zip"
+fi
+
+# download prebuilt /boot from arkOS (with light modification)
+if [ ! -f "${DIR_OUT}/ark-boot-RG351V_v2.0_09262021.zip" ];then
+  wget https://github.com/notnullgames/nullos/releases/download/rk-first/ark-boot-RG351V_v2.0_09262021.zip -O "${DIR_OUT}/ark-boot-RG351V_v2.0_09262021.zip"
+fi
+
+# clean up on exit
 function finish {
   sync
-  sudo umount "${DIR}/root/boot"
-  sudo umount "${DIR}/root"
-  sudo qemu-nbd --disconnect /dev/nbd0
+  sudo umount "${DIR_OUT}/root/boot"
+  sudo umount "${DIR_OUT}/root"
+  sudo qemu-nbd --disconnect "${DEVICE_NBD}"
 }
 trap finish EXIT
 
-sudo apt install -y build-essential debootstrap unzip git dosfstools qemu-utils
-
-if [ "$(uname -m)" == "aarch64" ]; then
-  BOOTSTRAP="debootstrap"
-else
-  sudo apt install -y binfmt-support qemu-user-static 
-  BOOTSTRAP="qemu-debootstrap --arch arm64"
-fi
-
+# build disk image
 qemu-img create -f qcow2 "${DISKFILE}" 2G
-
 sudo modprobe nbd max_part=8
-sudo qemu-nbd --connect=/dev/nbd0 "${DISKFILE}"
-
-cat << EOF | sudo sfdisk /dev/nbd1
-/dev/nbd0p1 : start=        2048, size=      204800, type=6
-/dev/nbd0p2 : start=      206848, size=     3987456, type=83
+sudo qemu-nbd --connect="${DEVICE_NBD}" "${DISKFILE}"
+cat << EOF | sudo sfdisk ${DEVICE_NBD}
+${DEVICE_NBD}p1 : start=        2048, size=      204800, type=6
+${DEVICE_NBD}p2 : start=      206848, size=     3987456, type=83
 EOF
-
-sudo mkfs -j /dev/nbd1p2
+sudo mkfs -j "${DEVICE_NBD}p2"
 sudo mkfs -t fat /dev/nbd0p1
-sudo mkdir -p root
-sudo mount /dev/nbd1p2 root
-sudo mkdir -p root/boot
-sudo mount /dev/nbd0p1 root/boot
+sudo mkdir -p "${DIR_OUT}/root"
+sudo mount "${DEVICE_NBD}p2" "${DIR_OUT}/root"
+sudo mkdir -p "${DIR_OUT}/root/boot"
+sudo mount "${DEVICE_NBD}p1" "${DIR_OUT}/root/boot"
 
-sudo ${BOOTSTRAP} bullseye root https://deb.debian.org/debian
+sudo debootstrap --arch arm64 bullseye root https://deb.debian.org/debian
 
-wget https://dn.odroid.com/RK3326/ODROID-GO-Advance/rk3326_r13p0_gbm_with_vulkan_and_cl.zip
-unzip rk3326_r13p0_gbm_with_vulkan_and_cl.zip
-sudo mkdir -p root/usr/local/lib/aarch64-linux-gnu/ root/usr/local/lib/arm-linux-gnueabihf/
-sudo mv libmali.so_rk3326_gbm_arm64_r13p0_with_vulkan_and_cl root/usr/local/lib/aarch64-linux-gnu/libmali-bifrost-g31-rxp0-gbm.so
-sudo mv libmali.so_rk3326_gbm_arm32_r13p0_with_vulkan_and_cl root/usr/local/lib/arm-linux-gnueabihf/libmali-bifrost-g31-rxp0-gbm.so
-rm rk3326_r13p0_gbm_with_vulkan_and_cl.zip
+cd "${DIR_OUT}"
+unzip "${DIR_OUT}/rk3326_r13p0_gbm_with_vulkan_and_cl.zip"
+sudo mkdir -p "${DIR_OUT}/root/usr/local/lib/aarch64-linux-gnu/" "${DIR_OUT}/root/usr/local/lib/arm-linux-gnueabihf/"
+sudo mv libmali.so_rk3326_gbm_arm64_r13p0_with_vulkan_and_cl "${DIR_OUT}/root/usr/local/lib/aarch64-linux-gnu/libmali-bifrost-g31-rxp0-gbm.so"
+sudo mv libmali.so_rk3326_gbm_arm32_r13p0_with_vulkan_and_cl "${DIR_OUT}/root/usr/local/lib/arm-linux-gnueabihf/libmali-bifrost-g31-rxp0-gbm.so"
+rm "${DIR_OUT}/rk3326_r13p0_gbm_with_vulkan_and_cl.zip"
 
-cd root/boot
-sudo unzip ../../ark-boot-RG351V_v2.0_09262021.zip
-cd ../..
+cd "${DIR_OUT}/root/boot"
+sudo unzip "${DIR_OUT}/ark-boot-RG351V_v2.0_09262021.zip"
